7773621ce056e3bb24ae5fdca66853a55694a134
Bimal Viswadas <bviswadas@zaizi.com>
2020-04-08 16:15:38 +0100

    conditional branching

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/conditionalres', function (req, res) {
+  var analyseChoice = req.session.data['analyseChoice']
+  var referOutcome = req.session.data['referoutcome']
+
+  if (analyseChoice === 'Refer') {
+    if (analyseChoice === 'Refer' && referOutcome === 'branded') {
+      res.redirect('branded')
+    } else if (analyseChoice === 'Refer' && referOutcome === 'unbranded') {
+      res.redirect('unbranded')
+    }
+  } else if (analyseChoice === 'image') {
+    res.redirect('image')
+  } else if (analyseChoice === 'home') {
+    res.redirect('home')
+  }
+})
+
 module.exports = router
